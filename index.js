649564259951856837e294e84dd64fa24d0f9328
michaljarnot <m.jarnot@yahoo.com>
2023-04-27 22:53:52 +0200

    feat: implement mouse move binding

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
-// TODO: this can be simplified and bindings dependency is not necessary
-const addon = require('bindings')({
-  bindings: 'robotcpp_node_bindings',
-  // TODO: figure out a better way to do this?
-  try: [['module_root', 'cpp', 'build', 'Release', 'bindings']]
-})
+const robotcpp = require('./cpp/build/Release/robotcpp_node_bindings.node');
+
+robotcpp.move({x: 100, y: 200});
+
+const Mouse = {
+    move: (point) => {
+        robotcpp.move(point);
+    },
+};
 
 module.exports = {
-  Keyboard: addon.Keyboard,
-  Mouse: addon.Mouse
+    Mouse,
 }
