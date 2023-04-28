865e88ca5754949ca614bf9d4f67f958f611d6cb
michaljarnot <m.jarnot@yahoo.com>
2023-04-28 03:00:07 +0200

    fix: remove namespace

--- index.d.ts
+++ index.d.ts
@@ -1,87 +1,85 @@
-declare module Robot {
-  type KeyCode = number;
+type KeyCode = number;
 
-  export enum SpecialKey {
-    BACKSPACE,
-    ENTER,
-    TAB,
-    ESCAPE,
-    UP,
-    DOWN,
-    RIGHT,
-    LEFT,
-    META,
-    ALT,
-    CONTROL,
-    SHIFT,
-    CAPSLOCK
-  }
+export enum SpecialKey {
+  BACKSPACE,
+  ENTER,
+  TAB,
+  ESCAPE,
+  UP,
+  DOWN,
+  RIGHT,
+  LEFT,
+  META,
+  ALT,
+  CONTROL,
+  SHIFT,
+  CAPSLOCK
+}
 
-  export class Keyboard {
-    static type(query: string): void;
+export class Keyboard {
+  static type(query: string): void;
 
-    static typeHumanLike(query: string): void;
+  static typeHumanLike(query: string): void;
 
-    static click(asciiChar: string | SpecialKey): void;
+  static click(asciiChar: string | SpecialKey): void;
 
-    static press(asciiChar: string | SpecialKey): void;
+  static press(asciiChar: string | SpecialKey): void;
 
-    static release(asciiChar: string | SpecialKey): void;
-  }
+  static release(asciiChar: string | SpecialKey): void;
+}
 
-  export enum MouseButton {
-    LEFT_BUTTON = 0,
-    RIGHT_BUTTON = 1,
-    CENTER_BUTTON = 2
-  }
+export enum MouseButton {
+  LEFT_BUTTON = 0,
+  RIGHT_BUTTON = 1,
+  CENTER_BUTTON = 2
+}
 
-  interface Point {
-    x: number;
-    y: number;
-  }
+interface Point {
+  x: number;
+  y: number;
+}
 
-  export class Mouse {
-    static delay: number;
+export class Mouse {
+  static delay: number;
 
-    static move(point: Point): void;
+  static move(point: Point): void;
 
-    static moveSmooth(point: Point, speed?: number): void;
+  static moveSmooth(point: Point, speed?: number): void;
 
-    static drag(point: Point, speed?: number): void;
+  static drag(point: Point, speed?: number): void;
 
-    static getPosition(): Point;
+  static getPosition(): Point;
 
-    static toggleButton(down: boolean, button: MouseButton, doubleClick?: boolean): void;
+  static toggleButton(down: boolean, button: MouseButton, doubleClick?: boolean): void;
 
-    static click(button: MouseButton): void;
+  static click(button: MouseButton): void;
 
-    static doubleClick(button: MouseButton): void;
+  static doubleClick(button: MouseButton): void;
 
-    static ScrollBy(y: number, x?: number): void;
-  }
+  static ScrollBy(y: number, x?: number): void;
+}
 
-  export interface DisplaySize {
-    width: number;
-    height: number;
-  }
+export interface DisplaySize {
+  width: number;
+  height: number;
+}
 
-  export interface Pixel {
-    r: number;
-    g: number;
-    b: number;
-  }
+export interface Pixel {
+  r: number;
+  g: number;
+  b: number;
+}
 
-  export class Screen {
-    constructor();
+export class Screen {
+  constructor();
 
-    getPixelColor(x: number, y: number): Pixel;
+  getPixelColor(x: number, y: number): Pixel;
 
-    getScreenSize(): DisplaySize;
+  getScreenSize(): DisplaySize;
 
-    capture(x?: number, y?: number, width?: number, height?: number): void;
+  capture(x?: number, y?: number, width?: number, height?: number): void;
 
-    getPixels(): Pixel[];
+  getPixels(): Pixel[];
 
-    saveAsPNG(filename: string): void;
-  }
+  saveAsPNG(filename: string): void;
 }
